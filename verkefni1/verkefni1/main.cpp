30f372991f70d97aef60b3286481e879c2deb564
Jón Sigurðsson <jonsig15@ru.is>
2015-11-26 10:59:37 +0000

    föll útfærð

--- verkefni1/verkefni1/main.cpp
+++ verkefni1/verkefni1/main.cpp
@@ -11,8 +11,29 @@ class Person {
         char name, gender;
         int yearOfBirth, yearOfDeath;
     public:
-        void setName;
-        void setGender;
-        void setyearOfBirth;
-        void setyearOfDeath;
+        void setName();
+        void setGender();
+        void setyearOfBirth();
+        void setyearOfDeath();
 };
+void Person::setName(){
+    cout << "Input name";
+    cin >> name;
+}
+void Person::setGender(){
+    cout << "Input gender";
+    cin >> gender;
+}
+void Person::setyearOfBirth(){
+    cout << "Input year of birth";
+    cin >> yearOfBirth;
+}
+void Person::setyearOfDeath(){
+    char input;
+    cout << "Is this person dead?" << endl <<  "y/n";
+    cin >> input;
+    if(input==y){
+        cout << "Input year of death: ";
+        cin >> yearOfDeath;
+    }else{ yearOfDeath = 0;}
+}
