d24377d00eda7b5d5e108b9f176cdd2f739b8246
Kristofer Andersen <kristofer15@ru.is>
2015-11-27 13:27:55 +0000

    smá breytingar á printToFile

--- verkefni1/verkefni1/main.cpp
+++ verkefni1/verkefni1/main.cpp
@@ -6,55 +6,56 @@
 
 using namespace std;
 
-void printToFile(const Person& a, const int& num);             //Adds Person to file
+void printToFile(const Person& a);      //Adds Person to file
 void addPerson();                       //Reads people to add to file
 void setPerson(Person& newGuy);         //calls all Person::set functions. Could be replaced by operator overloading
-void masterMenu();                      //First menu
+void mainMenu();                      //First menu
 int main()
 {
     int choice;
-    masterMenu();
+    mainMenu();
     cin >> choice;
     if(choice == 1)
         addPerson();
+    if(choice == 2 || choice == 3)
+        cout << "I'm not ready!" << endl;
+
     return 0;
 }
 
 void addPerson()
 {
-    int personNum = 0;
     char addMore;
     do
     {
         Person newGuy;
         setPerson(newGuy);
-        printToFile(newGuy, personNum);
+        printToFile(newGuy);
         cout << "Add a new person?" << endl << "y/n: ";
         cin >> addMore;
-        personNum++;
     }
     while(tolower(addMore) == 'y');  //"tolower(choice)" converts upper case to lower
 }
 
 void setPerson(Person& newGuy)
 {
-    cin.ignore();
+    cin.ignore();           //losnar við síðasta cin input
     newGuy.setName();
     newGuy.setGender();
     newGuy.setyearOfBirth();
     newGuy.setyearOfDeath();
 }
 
-void printToFile(const Person& a, const int& num)
+void printToFile(const Person& a)
 {
     ofstream outf;
     outf.open("pList.txt", ios::app);
     outf << a;     //"<<" is overloaded in Person class.
-    outf << num << endl;
+    outf << endl;
     outf.close();
 }
 
-void masterMenu()
+void mainMenu()
 {
     cout << "What do you want to do?" << endl
          << "(1) Add people." << endl
