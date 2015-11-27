208fdbe765623799dcc6cbdef8d658ed3f54d91d
Kristofer Andersen <kristofer15@ru.is>
2015-11-27 12:35:42 +0000

    fixa main.cpp

--- verkefni1/verkefni1/main.cpp
+++ verkefni1/verkefni1/main.cpp
@@ -6,36 +6,33 @@
 
 using namespace std;
 
-
 void printToFile(Person a);             //Adds Person to file
 void addPerson();                       //Reads people to add to file
 void setPerson(Person& newGuy);         //calls all Person::set functions. Could be replaced by operator overloading
-
+void options1();
 int main()
 {
-    char choice;
-    cout << "Add people?" << endl << "y/n: ";
+    int choice;
+    options1();
     cin >> choice;
-    if(tolower(choice) == 'y')          //"tolower(choice)" converts upper case to lower
+    if(choice == 1)
         addPerson();
     return 0;
 }
 
 void addPerson()
 {
-    ofstream outf;
-    outf.open("pList.txt", ios::app);
+
     char addMore;
     do
     {
         Person newGuy;
         setPerson(newGuy);
-        outf << newGuy;     //"<<" is overloaded in Person class.
+        printToFile(newGuy);
         cout << "Add a new person?" << endl << "y/n: ";
         cin >> addMore;
     }
-    while(tolower(addMore) == 'y');
-    outf.close();
+    while(tolower(addMore) == 'y');  //"tolower(choice)" converts upper case to lower
 }
 
 void setPerson(Person& newGuy)
@@ -44,4 +41,20 @@ void setPerson(Person& newGuy)
     newGuy.setGender();
     newGuy.setyearOfBirth();
     newGuy.setyearOfDeath();
+}
+
+void printToFile(Person a)
+{
+    ofstream outf;
+    outf.open("pList.txt", ios::app);
+    outf << a;     //"<<" is overloaded in Person class.
+    outf.close();
+}
+
+void options1()
+{
+    cout << "What do you want to do?" << endl
+         << "(1) Add people." << endl
+         << "(2) Search for people." << endl
+         << "(3) Show known people." << endl;
 }
