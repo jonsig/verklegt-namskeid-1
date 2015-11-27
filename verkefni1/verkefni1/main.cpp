d7476adb5a91b87d54aad04892c2edaa3f62d7e7
Kristofer Andersen <kristofer15@ru.is>
2015-11-27 13:02:22 +0000

    getline vandamál lagað, main menu bætt við

--- verkefni1/verkefni1/main.cpp
+++ verkefni1/verkefni1/main.cpp
@@ -6,14 +6,14 @@
 
 using namespace std;
 
-void printToFile(Person a);             //Adds Person to file
+void printToFile(const Person& a, const int& num);             //Adds Person to file
 void addPerson();                       //Reads people to add to file
 void setPerson(Person& newGuy);         //calls all Person::set functions. Could be replaced by operator overloading
-void options1();
+void masterMenu();                      //First menu
 int main()
 {
     int choice;
-    options1();
+    masterMenu();
     cin >> choice;
     if(choice == 1)
         addPerson();
@@ -22,36 +22,39 @@ int main()
 
 void addPerson()
 {
-
+    int personNum = 0;
     char addMore;
     do
     {
         Person newGuy;
         setPerson(newGuy);
-        printToFile(newGuy);
+        printToFile(newGuy, personNum);
         cout << "Add a new person?" << endl << "y/n: ";
         cin >> addMore;
+        personNum++;
     }
     while(tolower(addMore) == 'y');  //"tolower(choice)" converts upper case to lower
 }
 
 void setPerson(Person& newGuy)
 {
+    cin.ignore();
     newGuy.setName();
     newGuy.setGender();
     newGuy.setyearOfBirth();
     newGuy.setyearOfDeath();
 }
 
-void printToFile(Person a)
+void printToFile(const Person& a, const int& num)
 {
     ofstream outf;
     outf.open("pList.txt", ios::app);
     outf << a;     //"<<" is overloaded in Person class.
+    outf << num << endl;
     outf.close();
 }
 
-void options1()
+void masterMenu()
 {
     cout << "What do you want to do?" << endl
          << "(1) Add people." << endl
