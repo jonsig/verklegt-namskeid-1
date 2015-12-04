1d0660dafc33aa012f89262ba0e6abc407cb1f5f
Kristofer Andersen <kristofer15@ru.is>
2015-12-04 17:03:52 +0000

    skrifar núna í sql skrá, les enn úr txt

---
+++ week2/repositories/scientistrepository.cpp
@@ -0,0 +1,123 @@
+#include "repositories/scientistrepository.h"
+#include "utilities/utils.h"
+#include "utilities/constants.h"
+
+#include <fstream>
+#include <cstdlib>
+#include <QtSql>
+
+
+using namespace std;
+
+ScientistRepository::ScientistRepository()
+{
+    fileName = constants::DATA_FILE_NAME;
+}
+
+std::vector<Scientist> ScientistRepository::getAllScientists()
+{
+    ifstream file;
+
+    file.open(fileName.c_str());
+
+    vector<Scientist> scientists;
+
+    if (file.is_open())
+    {
+        string line;
+        while(getline(file, line))
+        {
+            vector<string> fields = utils::splitString(line, constants::FILE_DELIMETER);
+
+            if (fields.size() >= 3)
+            {
+                string name = fields.at(0);
+                enum sexType sex = utils::stringToSex(fields.at(1));
+                int yearBorn = utils::stringToInt(fields.at(2));
+
+                if (fields.size() == 3)
+                {
+                    scientists.push_back(Scientist(name, sex, yearBorn));
+                }
+                else
+                {
+                    int yearDied = utils::stringToInt(fields.at(3));
+
+                    scientists.push_back(Scientist(name, sex, yearBorn, yearDied));
+                }
+            }
+        }
+    }
+
+    file.close();
+
+    return scientists;
+}
+
+vector<Scientist> ScientistRepository::searchForScientists(string searchTerm)
+{
+    vector<Scientist> allScientists = getAllScientists();
+    vector<Scientist> filteredScientists;
+
+    for (unsigned int i = 0; i < allScientists.size(); i++)
+    {
+        if (allScientists.at(i).contains(searchTerm))
+        {
+            filteredScientists.push_back(allScientists.at(i));
+        }
+    }
+
+    return filteredScientists;
+}
+/* Disabled
+bool ScientistRepository::addScientist(Scientist scientist)
+{
+    ofstream file;
+
+    file.open(fileName.c_str(), std::ios::app);
+
+    if (file.is_open())
+    {
+        string name = scientist.getName();
+        enum sexType sex = scientist.getSex();
+        int yearBorn = scientist.getYearBorn();
+        int yearDied = scientist.getYearDied();
+
+        file << name << constants::FILE_DELIMETER
+             << sex << constants::FILE_DELIMETER
+             << yearBorn << constants::FILE_DELIMETER;
+
+        if (yearDied != constants::YEAR_DIED_DEFAULT_VALUE)
+        {
+            file << yearDied;
+        }
+
+        file << '\n';
+    }
+    else
+    {
+        return false;
+    }
+
+    file.close();
+    bool test = addScientistSql(scientist);
+    return true;
+}
+*/
+
+bool ScientistRepository::addScientist(Scientist scientist)
+{
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("scientistRepository.db");
+    if(db.open())
+    {
+        QSqlQuery query(db);
+        string sSex = utils::intToString(scientist.getSex());
+        string sYearBorn = utils::intToString(scientist.getYearBorn());
+        string sYearDied = utils::intToString(scientist.getYearDied());
+        string queryInsert = "INSERT INTO scientists(name,sex,yearBorn,yearDied)VALUES('"+scientist.getName()+"','"+sSex+"','"+sYearBorn+"','"+sYearDied+"')";
+        if(query.exec(QString(queryInsert.c_str())))
+            return true;
+    }
+    return false;
+}
