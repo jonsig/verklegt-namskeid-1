b3cec000d17273017a9ca17270179e522a3cfe8a
Kristofer Andersen <kristofer15@ru.is>
2015-12-09 07:58:26 +0000

    relation virkar aaaw yiss

--- week2/repositories/relationrepository.cpp
+++ week2/repositories/relationrepository.cpp
@@ -14,7 +14,62 @@ RelationRepository::RelationRepository()
     db.setDatabaseName(QString(constants::FILE_NAME.c_str()));
 }
 
-#include <iostream>
+vector<string> RelationRepository::findSciRelation(string name)
+{
+    vector<string> results;
+    results.push_back(name);
+
+    if(db.open())
+    {
+        QSqlQuery query(db);
+
+        string queryInsert = "SELECT c.name"
+                             " FROM compSciRelation sic"
+                             " JOIN computers c"
+                             " ON c.com_id = sic.com_id"
+                             " JOIN scientists s"
+                             " ON s.sci_id = sic.sci_id"
+                             " WHERE s.name LIKE '" + name +"'";
+
+        query.exec(QString(queryInsert.c_str()));
+        while(query.next())
+        {
+            printf("\nchecking next\n");
+            results.push_back(query.value("name").toString().toStdString());
+        }
+
+        db.close();
+    }
+    return results;
+}
+
+vector<string> RelationRepository::findCompRelation(string name)
+{
+    vector<string> results;
+    results.push_back(name);
+
+    if(db.open())
+    {
+        QSqlQuery query(db);
+
+        string queryInsert = "SELECT s.name"
+                             " FROM compSciRelation sic"
+                             " JOIN computers c"
+                             " ON c.com_id = sic.com_id"
+                             " JOIN scientists s"
+                             " ON s.sci_id = sic.sci_id"
+                             " WHERE c.name LIKE '" + name +"'";
+
+        query.exec(QString(queryInsert.c_str()));
+        while(query.next())
+        {
+            results.push_back(query.value("name").toString().toStdString());
+        }
+        db.close();
+    }
+    return results;
+}
+
 bool RelationRepository::addRelation(string compName, string sciName)
 {
     if(db.open())
@@ -24,24 +79,29 @@ bool RelationRepository::addRelation(string compName, string sciName)
                 "com_id INTEGER,"
                 "sci_id INTEGER,"
                 "FOREIGN KEY (com_id) REFERENCES computers(com_id),"
-                "FOREIGN KEY (sci_id) REFERENCES scientists(sci_id)"
+                "FOREIGN KEY (sci_id) REFERENCES scientists(sci_id),"
                 "PRIMARY KEY (com_id, sci_id)"
                 ")";
         query.exec(QString(queryInsert.c_str()));
 
         queryInsert = "SELECT com_id FROM computers WHERE name LIKE '" + compName + "'";
-        if(query.exec(QString(queryInsert.c_str())))
+        if(!query.exec(QString(queryInsert.c_str())))
+        {
+            db.close();
             return false;
+        }
         query.next();
         string compId = query.value("com_id").toString().toStdString();
 
         queryInsert = "SELECT sci_id FROM scientists WHERE name LIKE '" + sciName + "'";
-        if(query.exec(QString(queryInsert.c_str())))
+        if(!query.exec(QString(queryInsert.c_str())))
+        {
+            db.close();
             return false;
+        }
         query.next();
         string sciId = query.value("sci_id").toString().toStdString();
 
-        cout << endl << compId << " " << sciId << endl;
         queryInsert = "INSERT INTO compSciRelation VALUES ('" + compId + "','" + sciId + "')";
         if(query.exec(QString(queryInsert.c_str())))
         {
--- week2/repositories/relationrepository.h
+++ week2/repositories/relationrepository.h
@@ -15,6 +15,26 @@ class RelationRepository
 public:
     RelationRepository();
 
+    /**
+     * @brief findSciRelation finds all relations to a given scientist name
+     * @param name is the scientist name
+     * @return vector where the first value is the scientists name and the rest are results
+     */
+    vector<string> findSciRelation(string name);
+
+    /**
+     * @brief findCompRelation finds all relations to a given computer name
+     * @param name is the computer name
+     * @return vector where the first value is the computer name and the rest are results
+     */
+    vector<string> findCompRelation(string name);
+
+    /**
+     * @brief addRelation adds a relation between a computer and a scientist in the relation database
+     * @param compName name of the computer to be related
+     * @param sciName name of the scientist to be related
+     * @return true/false indicating success
+     */
     bool addRelation(string compName, string sciName);
 
 private:
